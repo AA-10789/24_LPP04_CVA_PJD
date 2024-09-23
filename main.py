72453c5971bb9ebe90b459ec698e2b6e30d758ff
AA-10789 <anri.arra@gmail.com>
2024-09-23 19:24:56 +0100

    Idade
    Versão com data de nascimento

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ Write your code in this editor and press "Run" button to execute it.
 
 '''
 
-idade = int(input("Introduza a sua idade: "))
+dia = 23
+mes = 9
+ano = 2024
+
+
+input = input("Introduza a sua data de nascimento (DD/MM/AAAA): ")
+dia_introduzido, mes_introduzido, ano_introduzido = map(int, input.split('/'))
+
+idade = ano - ano_introduzido
+
+if (mes_introduzido, dia_introduzido) > (mes, dia):
+    idade -= 1
+
 
 if idade < 0:
     print("Idade inválida.")
@@ -18,3 +30,6 @@ elif idade < 65:
     print("É um adulto. Maior de idade.")
 else:
     print("É um idoso. Maior de idade.")
+
+
+print(f"A sua idade é: {idade} anos.")
